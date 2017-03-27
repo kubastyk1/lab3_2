f89545a2319f9c6cafc5b6757f9c2135bcac003a
Sasho <Sasho>
2017-03-27 20:34:02 +0200

    Dodano zaleznosci oraz klase NewsLoaderTest

---
+++ src/test/java/edu/iis/mto/staticmock/NewsLoaderTest.java
@@ -0,0 +1,15 @@
+package edu.iis.mto.staticmock;
+
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by Sasho on 2017-03-27.
+ */
+@RunWith(PowerMockRunner.class)
+public class NewsLoaderTest {
+
+}
