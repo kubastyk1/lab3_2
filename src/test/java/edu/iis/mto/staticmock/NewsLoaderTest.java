ac0a66c6e0c2a12127bc674052c7d5a7fd9b14a9
Patryk Wierzyński <p.wierzynski@live.com>
2017-03-28 13:26:47 +0200

    updated dependencies required for tests and added NewsLoaderTest class

---
+++ src/test/java/edu/iis/mto/staticmock/NewsLoaderTest.java
@@ -0,0 +1,21 @@
+package edu.iis.mto.staticmock;
+
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * Created by Patryk Wierzyński
+ */
+
+public class NewsLoaderTest {
+	@Before
+	public void setUp() throws Exception {
+
+	}
+
+	@Test
+	public void loadNews() throws Exception {
+
+	}
+
+}
