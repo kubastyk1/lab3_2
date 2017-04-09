d0916f5d9a34178f40b9ee1e66d98e91a13ab9b5
Jakub Stawiszyński <kubastyk@wp.pl>
2017-04-09 22:37:38 +0200

    Added NewsLoaderTest class and powermock dependencies

---
+++ src/test/java/lab3_2/NewsLoaderTest.java
@@ -0,0 +1,22 @@
+package lab3_2;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import edu.iis.mto.staticmock.NewsLoader;
+
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest( NewsLoader.class )
+public class NewsLoaderTest {
+
+	@Test
+	public void test() {
+
+	}
+
+}
