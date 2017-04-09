8ecc664235967623ebad05da5719088ea395b17f
Jakub Stawiszyński <kubastyk@wp.pl>
2017-04-10 00:04:01 +0200

    Add setUp, update PublishableNews class

--- src/test/java/lab3_2/NewsLoaderTest.java
+++ src/test/java/lab3_2/NewsLoaderTest.java
@@ -1,22 +1,70 @@
 package lab3_2;
 
-import static org.junit.Assert.*;
+import static org.hamcrest.CoreMatchers.hasItem;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.junit.Assert.assertThat;
+import static org.powermock.api.mockito.PowerMockito.mock;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.when;
 
+import java.util.List;
+
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.reflect.Whitebox;
 
+import edu.iis.mto.staticmock.Configuration;
+import edu.iis.mto.staticmock.ConfigurationLoader;
+import edu.iis.mto.staticmock.IncomingInfo;
+import edu.iis.mto.staticmock.IncomingNews;
 import edu.iis.mto.staticmock.NewsLoader;
+import edu.iis.mto.staticmock.NewsReaderFactory;
+import edu.iis.mto.staticmock.PublishableNews;
+import edu.iis.mto.staticmock.SubsciptionType;
+import edu.iis.mto.staticmock.reader.NewsReader;
 
 
 @RunWith(PowerMockRunner.class)
-@PrepareForTest( NewsLoader.class )
+@PrepareForTest( {ConfigurationLoader.class, NewsReaderFactory.class, PublishableNews.class} )
 public class NewsLoaderTest {
 
-	@Test
-	public void test() {
+	private NewsLoader newsLoader;
+	private IncomingInfo pubInfo = new IncomingInfo("public", SubsciptionType.NONE);
+	private IncomingInfo subInfo = new IncomingInfo("subscription", SubsciptionType.A);
+	private String readerType = "readerType";
+	private List<String> publicContent;
+	private List<String> subscribentContent;
 
+	@Before
+	public void setUp(){
+
+		newsLoader = new NewsLoader();
+
+		mockStatic(ConfigurationLoader.class);
+		ConfigurationLoader configurationLoader = mock(ConfigurationLoader.class);
+		when(ConfigurationLoader.getInstance()).thenReturn(configurationLoader);
+
+		Configuration configuration = new Configuration();
+		Whitebox.setInternalState(configuration, "readerType", readerType);
+		when(configurationLoader.loadConfiguration()).thenReturn(configuration);
+
+		IncomingNews incomingNews = new IncomingNews();
+		incomingNews.add(pubInfo);
+		incomingNews.add(subInfo);
+
+		NewsReader newsReader = mock(NewsReader.class);
+		when(newsReader.read()).thenReturn(incomingNews);
+
+		mockStatic(NewsReaderFactory.class);
+		NewsReaderFactory newsReaderFactory = new NewsReaderFactory();
+		when(newsReaderFactory.getReader(readerType)).thenReturn(newsReader);
+
+		PublishableNews publishableNews = newsLoader.loadNews();
+        publicContent = Whitebox.getInternalState(publishableNews, "publicContent");
+        subscribentContent = Whitebox.getInternalState(publishableNews, "subscribentContent");
 	}
-
 }
